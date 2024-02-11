f5bff7755ca7ba9fc2d88d5ff01b48dfc1603418
Feynman <290154062@qq.com>
2024-02-11 19:36:58 +0800

    fix: 修复创建用户，登录时间空值问题。

--- api/user_api/user_create.go
+++ api/user_api/user_create.go
@@ -11,58 +11,58 @@ import (
 )
 
 type UserCreateRequest struct {
-	UserName string `json:"userName" binding:"required" label:"用户名"`// 用户名
-	Password string `json:"password" binding:"required"`// 密码
-	NickName string `json:"nickName"`// 昵称
-	RoleID uint `json:"roleID" binding:"required"`// 角色id
+	UserName string `json:"userName" binding:"required" label:"用户名"` // 用户名
+	Password string `json:"password" binding:"required"`             // 密码
+	NickName string `json:"nickName"`                                // 昵称
+	RoleID   uint   `json:"roleID" binding:"required"`               // 角色id
 }
 
-func (UserApi) UserCreateView(c *gin.Context){
+func (UserApi) UserCreateView(c *gin.Context) {
 	// 用来储存用户传参
-	var cr UserCreateRequest 
+	var cr UserCreateRequest
 	// 绑定json传参
 	err := c.ShouldBindJSON(&cr)
 	if err != nil {
-		res.FailWithError(err,c)
-		return 
+		res.FailWithError(err, c)
+		return
 	}
 	// 数据库操作
 	var user models.UserModel
 	err = global.DB.Take(&user, "userName = ?", cr.UserName).Error
 	if err == nil {
-		res.FailWithMsg("用户名已存在",c)
+		res.FailWithMsg("用户名已存在", c)
 		return
 	}
 
-	if cr.NickName == ""{
+	if cr.NickName == "" {
 		// 昵称不存在
 		var maxID uint
 		global.DB.Model(models.UserModel{}).Select("max(id)").Scan(&maxID)
-		cr.NickName = fmt.Sprintf("用户_%d",maxID+1)
+		cr.NickName = fmt.Sprintf("用户_%d", maxID+1)
 	}
 
 	// 判断角色id
 	var role models.RoleModel
 	err = global.DB.Take(&role, cr.RoleID).Error
-	if err != nil{
+	if err != nil {
 		global.Log.Error(err)
-		res.FailWithMsg("角色不存在",c)
+		res.FailWithMsg("角色不存在", c)
 		return
 	}
 
 	// 创建新用户
 	err = global.DB.Create(&models.UserModel{
 		UserName: cr.UserName,
-		Password:pwd.HashPwd(cr.Password),
-		NickName:cr.NickName,
-		IP:c.RemoteIP(),
-		RoleID:cr.RoleID,
+		Password: pwd.HashPwd(cr.Password),
+		NickName: cr.NickName,
+		IP:       c.RemoteIP(),
+		RoleID:   cr.RoleID,
 	}).Error
-	if err != nil{
+	if err != nil {
 		global.Log.Error(err)
-		res.FailWithMsg("用户创建失败",c)
+		res.FailWithMsg("用户创建失败", c)
 		return
 	}
-	res.OKWithMsg("success",c)
+	res.OKWithMsg("success", c)
 	return
 }
--- models/user_model.go
+++ models/user_model.go
@@ -1,19 +1,21 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+)
 
 // 用户表
 type UserModel struct {
 	Model
-	UserName  string    `gorm:"column:userName;size:36;unique;not null;comment:用户名" json:"-"` // 用户名
-	Password  string    `gorm:"column:password;size:128;comment:密码" json:"password"`          // 密码
-	Avatar    string    `gorm:"column:avatar;size:256;comment:头像" json:"avatar"`              // 头像
-	NickName  string    `gorm:"column:nickName;size:36;comment:昵称" json:"nickName"`           // 昵称
-	Email     string    `gorm:"column:email;size:128;comment:邮箱" json:"email"`                // 邮箱
-	Token     string    `gorm:"column:token;size:64;comment:其他平台的唯一id" json:"-"`              // 其他平台的唯一id
-	IP        string    `gorm:"column:ip;size:16;comment:ip" json:"ip"`                       // ip
-	Address   string    `gorm:"column:address;size:64;comment:地址" json:"address"`             // 地址
-	RoleID    uint      `gorm:"column:roleID;comment:用户对应的角色id" json:"roleID"`                // 用户对应的角色id
-	LastLogin time.Time `gorm:"column:lastLogin" json:"lastLogin"`                            //用户最近一次登录时间
-	RoleModel RoleModel `gorm:"foreignKey:RoleID" json:"-"`                           // json:"roleModel"
+	UserName  string       `gorm:"column:userName;size:36;unique;not null;comment:用户名" json:"-"` // 用户名
+	Password  string       `gorm:"column:password;size:128;comment:密码" json:"password"`          // 密码
+	Avatar    string       `gorm:"column:avatar;size:256;comment:头像" json:"avatar"`              // 头像
+	NickName  string       `gorm:"column:nickName;size:36;comment:昵称" json:"nickName"`           // 昵称
+	Email     string       `gorm:"column:email;size:128;comment:邮箱" json:"email"`                // 邮箱
+	Token     string       `gorm:"column:token;size:64;comment:其他平台的唯一id" json:"-"`              // 其他平台的唯一id
+	IP        string       `gorm:"column:ip;size:16;comment:ip" json:"ip"`                       // ip
+	Address   string       `gorm:"column:address;size:64;comment:地址" json:"address"`             // 地址
+	RoleID    uint         `gorm:"column:roleID;comment:用户对应的角色id" json:"roleID"`                // 用户对应的角色id
+	LastLogin sql.NullTime `gorm:"column:lastLogin" json:"lastLogin"`                            //用户最近一次登录时间
+	RoleModel RoleModel    `gorm:"foreignKey:RoleID" json:"-"`                                   // json:"roleModel"
 }
